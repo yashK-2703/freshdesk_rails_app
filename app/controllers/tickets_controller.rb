11d54df7f097b62b6e0a8a2e0eb856228759e794
yashK-2703 <yashkedia274@gmail.com>
2023-04-18 09:54:17 +0000

    Check for existing tickets

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -15,39 +15,74 @@ class TicketsController < ApplicationController
       tickets = JSON.parse(response.body)
 
       tickets.each do |ticket|
-        Ticket.create!(
-          cc_emails: ticket['cc_emails'],
-          fwd_emails: ticket['fwd_emails'],
-          reply_cc_emails: ticket['reply_cc_emails'],
-          ticket_cc_emails: ticket['ticket_cc_emails'],
-          fr_escalated: ticket['fr_escalated'],
-          spam: ticket['spam'],
-          email_config_id: ticket['email_config_id'],
-          group_id: ticket['group_id'],
-          priority: ticket['priority'],
-          requester_id: ticket['requester_id'],
-          responder_id: ticket['responder_id'],
-          source: ticket['source'],
-          company_id: ticket['company_id'],
-          status: ticket['status'],
-          subject: ticket['subject'],
-          association_type: ticket['association_type'],
-          support_email: ticket['support_email'],
-          to_emails: ticket['to_emails'],
-          product_id: ticket['product_id'],
-          ticket_id: ticket['id'],
-          type: ticket['type'],
-          due_by: ticket['due_by'],
-          fr_due_by: ticket['fr_due_by'],
-          is_escalated: ticket['is_escalated'],
-          custom_fields: ticket['custom_fields'],
-          created_at: ticket['created_at'],
-          updated_at: ticket['updated_at'],
-          associated_tickets_count: ticket['associated_tickets_count'],
-          tags: ticket['tags'],
-          nr_due_by: ticket['nr_due_by'],
-          nr_escalated: ticket['nr_escalated']
-        )
+        existing_ticket = Ticket.find_by(ticket_id: ticket['id'])
+
+        if existing_ticket
+          existing_ticket.update!(
+            cc_emails: ticket['cc_emails'],
+            fwd_emails: ticket['fwd_emails'],
+            reply_cc_emails: ticket['reply_cc_emails'],
+            ticket_cc_emails: ticket['ticket_cc_emails'],
+            fr_escalated: ticket['fr_escalated'],
+            spam: ticket['spam'],
+            email_config_id: ticket['email_config_id'],
+            group_id: ticket['group_id'],
+            priority: ticket['priority'],
+            requester_id: ticket['requester_id'],
+            responder_id: ticket['responder_id'],
+            source: ticket['source'],
+            company_id: ticket['company_id'],
+            status: ticket['status'],
+            subject: ticket['subject'],
+            association_type: ticket['association_type'],
+            support_email: ticket['support_email'],
+            to_emails: ticket['to_emails'],
+            product_id: ticket['product_id'],
+            type: ticket['type'],
+            due_by: ticket['due_by'],
+            fr_due_by: ticket['fr_due_by'],
+            is_escalated: ticket['is_escalated'],
+            custom_fields: ticket['custom_fields'],
+            created_at: ticket['created_at'],
+            updated_at: ticket['updated_at'],
+            associated_tickets_count: ticket['associated_tickets_count'],
+            tags: ticket['tags'],
+            nr_due_by: ticket['nr_due_by']
+          )
+        else
+          Ticket.create!(
+            cc_emails: ticket['cc_emails'],
+            fwd_emails: ticket['fwd_emails'],
+            reply_cc_emails: ticket['reply_cc_emails'],
+            ticket_cc_emails: ticket['ticket_cc_emails'],
+            fr_escalated: ticket['fr_escalated'],
+            spam: ticket['spam'],
+            email_config_id: ticket['email_config_id'],
+            group_id: ticket['group_id'],
+            priority: ticket['priority'],
+            requester_id: ticket['requester_id'],
+            responder_id: ticket['responder_id'],
+            source: ticket['source'],
+            company_id: ticket['company_id'],
+            status: ticket['status'],
+            subject: ticket['subject'],
+            association_type: ticket['association_type'],
+            support_email: ticket['support_email'],
+            to_emails: ticket['to_emails'],
+            product_id: ticket['product_id'],
+            ticket_id: ticket['id'],
+            type: ticket['type'],
+            due_by: ticket['due_by'],
+            fr_due_by: ticket['fr_due_by'],
+            is_escalated: ticket['is_escalated'],
+            custom_fields: ticket['custom_fields'],
+            created_at: ticket['created_at'],
+            updated_at: ticket['updated_at'],
+            associated_tickets_count: ticket['associated_tickets_count'],
+            tags: ticket['tags'],
+            nr_due_by: ticket['nr_due_by']
+          )
+        end
       end
 
       flash[:success] = 'Tickets fetched successfully!'
@@ -55,7 +90,7 @@ class TicketsController < ApplicationController
       flash[:error] = 'Unable to fetch tickets'
     end
 
-    render json: tickets 
+    render json: tickets
   end
 end
 
